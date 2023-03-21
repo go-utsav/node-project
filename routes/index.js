206ea1f0dfccccb83f371192c7d979426abd01a1
go-utsav <utsavgohel3@gmail.com>
2023-03-21 15:44:49 +0530

    .

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {page:'Home', menuId:'home'});
+router.get('/', function (req, res, next) {
+  res.render('index', { page: 'Home', menuId: 'home' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', {page:'About Us', menuId:'about'});
+router.get('/about', function (req, res, next) {
+  res.render('about', { page: 'About Us', menuId: 'about' });
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', {page:'Contact Us', menuId:'contact'});
+router.get('/contact', function (req, res, next) {
+  res.render('contact', { page: 'Contact Us', menuId: 'contact' });
+});
+
+router.get('/utsav', function (req, res, next) {
+  res.render('utsav', { page: 'utsav', menuId: 'utsav' });
 });
 
 module.exports = router;
